5f32648b7e0fe4c751d633927c0c35e96c635a38
Reqyan Lazuardi <reqyan.1103@gmail.com>
2023-01-07 02:00:34 +0700

    first commit of this repository
    just commiting the code

---
+++ server.js
@@ -0,0 +1,23 @@
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+  });
+
+  server.route({
+    method: "POST",
+    path: "/login",
+    handler: (request, h) => {
+      const { username, password } = request.payload;
+      return `Welcome ${username}!`;
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
